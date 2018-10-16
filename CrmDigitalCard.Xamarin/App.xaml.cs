982836c0b88205f4ba43677676782648eee45813
Francesco Calore <francesco.calore@sbsoft.com>
2018-10-16 17:21:32 +0200

    Add project files.

---
+++ CrmDigitalCard.Xamarin/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace CrmDigitalCard.Xamarin
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            var tabs = new TabbedPage();
+
+            //tabs.Children.Add(new LocalHtml { Title = "Local" });
+            //tabs.Children.Add(new LocalHtmlBaseUrl { Title = "BaseUrl" });
+            tabs.Children.Add(new MainPage { Title = "Web Page" });
+            //tabs.Children.Add(new WebAppPage { Title = "External" });
+
+            MainPage = tabs;
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ CrmDigitalCard.Xamarin/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CrmDigitalCard.Xamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            var browser = new WebView();
+            browser.Source = "https://mockupstock.poweraddon.com";
+            Content = browser;
+        }
+    }
+}
